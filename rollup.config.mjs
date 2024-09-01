a8765094a4d009dc73b3e2e7a8f3d0808bfce9e9
柴惠滨 <909459963@qq.com>
2024-09-01 23:39:47 +0800

    打包配置

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,10 @@
 // rollup.config.mjs
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json';
 
 export default {
 	input: 'src/index.ts',
@@ -10,9 +14,18 @@ export default {
       format: 'esm'
     },
     // {
+    //   name: 'qrcodeKit',
     //   file: 'dist/index.js',
-    //   format: 'umd'
+    //   format: 'umd',
     // }
   ],
-  plugins: [typescript({declarationDir: 'dist'}), nodeResolve()]
+  plugins: [
+    commonjs(),
+    typescript({declarationDir: 'dist'}), 
+    /** 
+     * 可以将npm包源码一并打包
+     */
+    // nodeResolve(), 
+    // peerDepsExternal(),
+  ],
 };
